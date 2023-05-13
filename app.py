2050e373f6512feffff1a6ae641bf62374a3f4f0
Érick Hideki Oyakawa Awata <erickhoawata@gmail.com>
2023-05-12 21:12:34 -0300

    falta o css da tabela

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request ,render_template, make_response
+from flask import Flask, request ,render_template
 
 app = Flask("__name__")
 
@@ -9,100 +9,30 @@ def index():
 @app.route("/artefatos")
 def artefatos():
     return render_template("artefatos.html")
-
 @app.route("/avaliacao", methods=["GET","POST"])
 def avaliacao():
+    metodo = request.method
     if request.method == "POST":
         
         try:
-            # Para os nomes:
-            n1 = str(request.form.get("n1"))
-            n2 = str(request.form.get("n2"))
-            n3 = str(request.form.get("n3"))
-            n4 = str(request.form.get("n4"))
-            n5 = str(request.form.get("n5"))
-            n6 = str(request.form.get("n6"))
-            n7 = str(request.form.get("n7"))
-            # Pegando a b c da linha 1 
-            a1 = int(request.form.get("a1"))
-            b1 = int(request.form.get("b1"))
-            c1 = int(request.form.get("c1"))
-            # achando o d da linha 1
-            d1 = a1*0.3+b1*0.3+c1*0.4
-            d1 = int(d1)
-            # devolvendo o d para seu lugar
-            
-        
-            
-            # Pegando a b c da linha 2
-            a2 = int(request.form.get("a2"))
-            b2 = int(request.form.get("b2"))
-            c2 = int(request.form.get("c2"))
-            # achando o d da linha 2
-            d2 = a2*0.3+b2*0.3+c2*0.4
-            d2 = int(d2)
-            # devolvendo o d para seu lugar
-            
-        
-        
-            # Pegando a b c da linha 3 
-            a3 = int(request.form.get("a3"))
-            b3 = int(request.form.get("b3"))
-            c3 = int(request.form.get("c3"))
-            # achando o d da linha 3
-            d3 = a3*0.3+b3*0.3+c3*0.4
-            d3 = int(d3)
-            # devolvendo o d para seu lugar
-            
-        
-    
-            # Pegando a b c da linha 4
-            a4 = int(request.form.get("a4"))
-            b4 = int(request.form.get("b4"))
-            c4 = int(request.form.get("c4"))
-            # achando o d da linha 4
-            d4 = a4*0.3+b4*0.3+c4*0.4
-            d4 = int(d4)
-            # devolvendo o d para seu lugar
-            
+            pessoas = []
+            for c in range(1,8):
+                n = str(request.form.get(f"n{c}"))
+                a = float(request.form.get(f"a{c}"))
+                b = float(request.form.get(f"b{c}"))
+                c = float(request.form.get(f"c{c}"))
+                # achando o d da linha 1
+                d = a*0.3+b*0.3+c*0.4
+                d = float(d)
+                pessoas.append([n, a, b, c, d])
 
-        
-            # Pegando a b c da linha 5
-            a5 = int(request.form.get("a5"))
-            b5 = int(request.form.get("b5"))
-            c5 = int(request.form.get("c5"))
-            # achando o d da linha 5
-            d5 = a5*0.3+b5*0.3+c5*0.4
-            d5 = int(d5)
-            # devolvendo o d para seu lugar
-            
-        
-    
-            # Pegando a b c da linha 6
-            a6 = int(request.form.get("a6"))
-            b6 = int(request.form.get("b6"))
-            c6 = int(request.form.get("c6"))
-            # achando o d da linha 1
-            d6 = a6*0.3+b6*0.3+c6*0.4
-            d6 = int(d6)
-            # devolvendo o d para seu lugar
-            
-        
-    
-            # Pegando a b c da linha 7
-            a7 = int(request.form.get("a7"))
-            b7 = int(request.form.get("b7"))
-            c7 = int(request.form.get("c7"))
-            # achando o d da linha 7
-            d7 = a7*0.3+b7*0.3+c7*0.4
-            d7 = int(d7)
-            # devolvendo o d para seu lugar
-            return render_template("Avaliações.html", d7 = d7, d1 = d1, d2 = d2, d3 = d3, d4 = d4, d5 = d5, d6 = d6, n1=n1, n2=n2, n3=n3, n4=n4, n5=n5, n6=n6, n7=n7)
+            # devolvendo o d para seu lugar               
+            return render_template("Avaliações.html", pessoas=pessoas, metodo=metodo)
         except ValueError:
-            return render_template("Avaliações.html")
+            return render_template("Avaliações.html",  pessoas=pessoas, metodo=metodo)
     else:
         # if no POST method is requested render only the page.
-        return render_template("Avaliações.html")
+        return render_template("Avaliações.html", metodo=metodo)
 
 
 @app.route("/equipe")
