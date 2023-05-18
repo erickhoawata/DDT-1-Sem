0802b49e0f6cfc2a7f7cc9d7ec2ae215aa55dff6
kajiyap <kajiyapedro@gmail.com>
2023-05-17 21:39:38 -0300

    tabela PACER quase pronta
    Falta a descrição de como usar

--- app.py
+++ app.py
@@ -16,19 +16,35 @@ def avaliacao():
         
         try:
             pessoas = []
+            nota_usada = 0
+            nota_prof = float(request.form.get('nota_prof'))
+            nota_prof = int(nota_prof)
             for linha in range(1,8):
-                n = str(request.form.get(f"n{linha}"))
-                a = float(request.form.get(f"a{linha}"))
-                b = float(request.form.get(f"b{linha}"))
-                c = float(request.form.get(f"c{linha}"))
-                d = float(request.form.get(f"c{linha}"))
-                
-                pessoas.append([n, a, b, c, d])
+                #pegando nome:
+                nome = str(request.form.get(f"nome{linha}"))
 
-            # devolvendo o d para seu lugar               
-            return render_template("Avaliações.html", pessoas=pessoas, metodo=metodo)
+                #pegando as notas:
+                nota_proat = float(request.form.get(f"nota_proat{linha}"))
+                nota_auto = float(request.form.get(f"nota_auto{linha}"))
+                nota_colab = float(request.form.get(f"nota_colab{linha}"))
+                nota_prod = float(request.form.get(f"nota_prod{linha}"))
+
+                #somando as notas utilizadas
+                soma_notas = nota_proat + nota_auto + nota_colab + nota_prod
+                nota_usada =  nota_usada + soma_notas
+                
+
+                #lista para criar tabela:
+                pessoas.append([nome, int(nota_proat), int(nota_auto), int(nota_colab), int(nota_prod)])
+
+
+            
+                
+            # devolvendo o d para seu lugar   
+            nota_usada = int(nota_usada)
+            return render_template("Avaliações.html", pessoas=pessoas, metodo=metodo, nota_usada=nota_usada, nota_prof=nota_prof)
         except ValueError:
-            return render_template("Avaliações.html",  pessoas=pessoas, metodo=metodo)
+            return render_template("Avaliações.html", metodo=metodo)
     else:
         # if no POST method is requested render only the page.
         return render_template("Avaliações.html", metodo=metodo)
